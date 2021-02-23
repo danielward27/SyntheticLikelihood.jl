82faf31a4a66a71fd0c81d3039d92ffe7d01ccc7
danielward27 <danielward27@outlook.com>
2021-02-23 14:55:31 +0000

    Make peturb take ::Sampleable object

--- test/utils_test.jl
+++ test/utils_test.jl
@@ -1,27 +1,17 @@
-using SyntheticLikelihood, Test, Statistics, LinearAlgebra
+using SyntheticLikelihood, Test, Statistics, LinearAlgebra, Distributions
 
 ## Test peturb
 true_mean = [1.,1000]
-true_cov = [50 10; 10 50]
-peturbed = peturb(true_mean, true_cov, 1000)
+true_cov = [9 0; 9 0]
 
-sample_mean = mean.(eachcol(peturbed))
-sample_cov = cov(peturbed)
-
-# Specified with matrix
-@test_throws AssertionError peturb([1,2], rand(3,3))
-@test isapprox(true_mean, sample_mean; atol = 3)
-@test isapprox(true_cov, sample_cov; atol = 20)
-
-# Specified with vector
-true_cov = [50, 10]
-peturbed = peturb(true_mean, true_cov, 1000)
+d = MvNormal(2, 3) # Zero mean sd 5
+peturbed = peturb(true_mean, d, 1000)
 
 sample_mean = mean.(eachcol(peturbed))
 sample_cov = cov(peturbed)
 
 @test isapprox(true_mean, sample_mean; atol = 3)
-@test isapprox(diagm(true_cov), sample_cov; atol = 20)
+@test isapprox(true_cov, sample_cov; atol = 20)  # Note uses norm
 
 ## Test pairwise combinations
 pc = SyntheticLikelihood.pairwise_combinations
