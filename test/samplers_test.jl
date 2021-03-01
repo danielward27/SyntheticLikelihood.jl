8299ccf728cfa71b533941bfd1439d911ab1b84a
danielward27 <danielward27@outlook.com>
2021-03-01 10:57:30 +0000

    Add test for langevin diffusion and data collector

--- test/samplers_test.jl
+++ test/samplers_test.jl
@@ -1,1 +1,24 @@
-using SyntheticLikelihood, Test
+using SyntheticLikelihood, Test, Random, Distributions
+
+Random.seed!(1)
+
+# Sample from MVN
+d = MvNormal([10 5; 5 10])
+init_θ = [-15, -15]
+objective(θ) = logpdf(d, θ)
+gradient(θ) = gradlogpdf(d, θ)
+n_steps = 1000
+
+state = GradientState(init_θ, objective(init_θ), gradient(init_θ))
+
+data, state = LangevinDiffusion(;
+        state, objective, gradient, step_size = 1,
+        ξ = MvNormal([0.5 0; 0 0.5]), n_steps,
+        data_to_collect = [:θ, :counter])
+
+θ_samples = reduce(hcat, data[:θ])'
+θ_samples = θ_samples[100:end, :]  # Remove burn in
+counter = data[:counter]
+
+@test isapprox(mean.(eachcol(θ_samples)), [0, 0]; atol = 2)
+@test counter == Vector(1:n_steps)
